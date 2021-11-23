5fa2e3fdf57cf4bd03b5859d8243008832ae24c3
SooWanKim <herosdoor@paran.com>
2021-11-23 11:56:55 +0900

    add LoadingCircleUI.cs

---
+++ BookSearchApp/Assets/Scripts/UI/LoadingCircleUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LoadingCircleUI : MonoBehaviour
+{
+    public RectTransform progressRectComponent;
+    private float rotateSpeed = 200f;
+
+
+    private void Update()
+    {
+        progressRectComponent.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+}
